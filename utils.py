7307d06ad27a7b9d7493c55bbe57ac885011c008
thomasmuntzer <thomasmuntzer@users.noreply.github.com>
2021-07-19 00:39:53 +0200

    remove plots

--- utils.py
+++ utils.py
@@ -5,37 +5,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 import pandas as pd
+from typing import Union, List
 
-def createXYPlot(dfplot,
-                 y,
-                 today,
-                 plots_folder,
-                 x="giorno",
-                 xtitle=None,
-                 dpis=100,
-                 bar=False,
-                 barWidth=0.75,
-                 barStart=None,
-                 alpha=1.0,
-                 linewidth=1.0,
-                 savename=None,
-                 title=None,
-                 color=None,
-                 color_avg="orange",
-                 fitGauss=False, 
-                 startDate=None,
-                 xLim=None,
-                 yticks=None,
-                 daysInterval = 4):
+def createXYPlot(dfplot: pd.DataFrame,
+                 x: str,
+                 y: Union[str, List[str]],
+                 today: str,
+                 plots_folder: str,
+                 figsize_x: int = 10,
+                 figsize_y: int = 5,
+                 xtitle: str = None,
+                 ytitle: str = None,
+                 dpis: int = 100,
+                 bar: bool = False,
+                 bar_width: float = 0.75,
+                 bar_start: List[float] = None,
+                 alpha: float = 1.0,
+                 linewidth: float = 1.0,
+                 savename: str = None,
+                 title: str = None,
+                 color: str = None,
+                 start_date: str = None,
+                 xlim: float = None,
+                 days_interval: int = 4):
     
        
-    if startDate is not None: dfplot = dfplot[pd.to_datetime(dfplot[x]) > datetime.strptime(startDate,'%Y-%m-%d')]
+    if start_date is not None: dfplot = dfplot[pd.to_datetime(dfplot[x]) > datetime.strptime(start_date,'%Y-%m-%d')]
 
     dfplot = dfplot.sort_values(x).reset_index() 
     first_day = str(dfplot[x].tolist()[0])
-    fig, ax = plt.subplots(figsize=(12,6),dpi=dpis)
+    fig, ax = plt.subplots(figsize=(figsize_x, figsize_y),dpi=dpis)
     
-    xindexes = [ x for x in dfplot.index  if (x + 1) % daysInterval == 0]
+    xindexes = [ x for x in dfplot.index  if (x + 1) % days_interval == 0]
     xlabels =  [ str(dfplot[x].tolist()[i]) for i in xindexes]
 
     i=0
@@ -43,14 +44,9 @@ def createXYPlot(dfplot,
         x_data = dfplot.index
         y_data = np.array(dfplot[y_name])
         if bar:
-            plt.bar(x_data+barStart[i]*barWidth, y_data, width=barWidth, align="center", alpha=alpha, color=color, label=y_name)
+            plt.bar(x_data + bar_start[i] * bar_width, y_data, width=bar_width, align="center", alpha=alpha, color=color, label=y_name)
         else:
             plt.plot(x_data, y_data, alpha=alpha, color=color, linewidth=linewidth, label=y_name)
-        try:
-            y_data_mean = np.array(dfplot[y+"_media_7"])
-            plt.plot(x_data, y_data_mean,"-",color=color_avg,linewidth=2.5)
-        except:
-            pass
         i+=1
         
     plt.grid(which="both")
@@ -58,6 +54,8 @@ def createXYPlot(dfplot,
     
     if xtitle: 
         plt.xlabel(xtitle,fontsize=14)
+    if ytitle:
+        plt.ylabel(ytitle,fontsize=14)
     if title: 
         plt.title(title,fontsize=16)
     # format the coords message box
@@ -69,7 +67,8 @@ def createXYPlot(dfplot,
                rotation_mode="anchor", 
                verticalalignment = "top",
                horizontalalignment = "right")
-    plt.yticks(yticks,fontsize=12)
+    plt.yticks(fontsize=12)
+    
     if savename:
         plt.savefig(f"{plots_folder}/{savename}",bbox_inches="tight")
     plt.show()
