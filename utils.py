22870edf46ce8f2ec281c6e05a7ab7a15446a992
thomasmuntzer <thomasmuntzer@users.noreply.github.com>
2021-07-19 00:39:53 +0200

    adding notebook stuff

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ def createXYPlot(dfplot: pd.DataFrame,
                  y: Union[str, List[str]],
                  today: str,
                  plots_folder: str,
+                 error = False,
                  figsize_x: int = 10,
                  figsize_y: int = 5,
                  xtitle: str = None,
@@ -43,8 +44,23 @@ def createXYPlot(dfplot: pd.DataFrame,
     for y_name in y:
         x_data = dfplot.index
         y_data = np.array(dfplot[y_name])
+        if error: 
+            y_err = np.array(dfplot["err_" + y_name])
+            print
+        else:
+            y_err=None
         if bar:
-            plt.bar(x_data + bar_start[i] * bar_width, y_data, width=bar_width, align="center", alpha=alpha, color=color, label=y_name)
+            plt.bar(x_data + bar_start[i] * bar_width, 
+                         y_data, 
+                         yerr=y_err,
+                         xerr=None,
+                         width=bar_width, 
+                         align="center", 
+                         alpha=alpha, 
+                         color=color, 
+                         label=y_name,
+                         error_kw={"capsize":1.5,"elinewidth":1}
+                   )
         else:
             plt.plot(x_data, y_data, alpha=alpha, color=color, linewidth=linewidth, label=y_name)
         i+=1
@@ -75,3 +91,34 @@ def createXYPlot(dfplot: pd.DataFrame,
     plt.close()
     
     del(fig)
+    
+    
+    
+def get_efficacy(df: pd.DataFrame, z_value=1.645):
+    
+    for vax_status in ["1st_dose", "2nd_dose", "no_vax"]:
+        
+        df[f"ev_{vax_status}_per_100k"] =  np.round((1e5) * df[f"event_{vax_status}"]/df[f"pop_{vax_status}"],3)
+            
+        p = df[f"event_{vax_status}"]/df[f"pop_{vax_status}"]
+        q = 1 - p
+        N = df[f"pop_{vax_status}"]
+        df[f"err_ev_{vax_status}_per_100k"] = z_value * (1e5) * np.sqrt(p*q) / np.sqrt(N)
+        
+        
+    for vax_status in ["1st_dose", "2nd_dose"]:
+        df[f"efficacy_{vax_status}"] =  np.round(1 - df[f"ev_{vax_status}_per_100k"]/df["ev_no_vax_per_100k"],3)
+        
+        df.replace([-np.inf], 0, inplace=True)
+        
+        A = 1 - df[f"ev_{vax_status}_per_100k"]
+        B = df["ev_no_vax_per_100k"]
+        sA = df[f"err_ev_{vax_status}_per_100k"]
+        sB = df[f"err_ev_no_vax_per_100k"]
+        f = df[f"efficacy_{vax_status}"]
+        df[f"err_efficacy_{vax_status}"] = z_value * np.round(np.abs(f) * np.sqrt((sA/A)**2 + (sB/B)**2),3)
+            
+        df[f"arr_{vax_status}"] = (df[f"ev_no_vax_per_100k"] - df[f"ev_{vax_status}_per_100k"])/(1e5)
+        df[f"nntv_{vax_status}"] = 1/df[f"arr_{vax_status}"]
+    
+    return df
